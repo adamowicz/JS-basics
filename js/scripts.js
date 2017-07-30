4ebd285b4715479e84c65d39aec9188a816ed414
kaktus83 <adamowicz.anna@gmail.com>
2017-07-30 21:30:15 +0200

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter value "a"');
+    h = prompt('Enter value "h"');
+    triangleArea = a * h / 2;
+alert('Triangle field with base a=' + a + ' and height h=' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a=' + a + ' and height h=' + h + ' is equal to: ' + triangleArea);
